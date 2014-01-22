9d1736f6e12aea3e8e582eb6a1cd49e5cb99efe3
dylan2013 <dylan.huang@ischool.com.tw>
2014-01-22 15:48:41 +0800

    新增一堆搞不懂的檔案內容

--- 017客戶服務網站測試用/017客戶服務網站測試用/js/TestJScript.js
+++ 017客戶服務網站測試用/017客戶服務網站測試用/js/TestJScript.js
@@ -1,3 +1,6 @@
-﻿$namespace(function () {
+﻿var templateHeader = '<div class="content"><a href="/categories/12128-Documentation" class="cta"><span></span><h3>Read our docs</h3><p>Learn how to set up and use Zendesk</p></a><a href="https://support.zendesk.com/forums/1847-Community-questions-answers" class="cta"><span></span><h3>Ask the community</h3><p>Join customer service pros in community Q&A</p></a><a href="https://support.zendesk.com/forums/3199-Community-tips-tricks" class="cta last"><span></span><h3>Share a tip</h3><p>Help the community with tips and examples</p></a></div>';
+var templateFooter = '';
+
+$namespace(function () {
     var landingItems = '<div class="content"><a href="/categories/12128-Documentation" class="cta"><span></span><h3>Read our docs</h3><p>Learn how to set up and use Zendesk</p></a><a href="https://support.zendesk.com/forums/1847-Community-questions-answers" class="cta"><span></span><h3>Ask the community</h3><p>Join customer service pros in community Q&A</p></a><a href="https://support.zendesk.com/forums/3199-Community-tips-tricks" class="cta last"><span></span><h3>Share a tip</h3><p>Help the community with tips and examples</p></a></div>';
 });
